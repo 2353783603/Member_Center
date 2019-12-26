f3450e4c2559de121ae781edcda2f4a27f9c6f12
liudetao <dtliu@cheweilai.cn>
2019-12-26 15:31:38 +0800

    dtliu_first_modify

---
+++ src/components/mainTabbar/MainTabBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home" activeColor="pink">
+      <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg" alt="">
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item path="/category" activeColor="pink">
+      <img slot="item-icon" src="../../assets/img/tabbar/category.svg" alt="">
+      <img slot="item-icon-active" src="../../assets/img/tabbar/category_active.svg" alt="">
+      <div slot="item-text">健康资讯</div>
+    </tab-bar-item>
+    <tab-bar-item path="/cart" activeColor="pink">
+      <img slot="item-icon" src="../../assets/img/tabbar/shopcart.svg" alt="">
+      <img slot="item-icon-active" src="../../assets/img/tabbar/shopcart_active.svg" alt="">
+      <div slot="item-text">消息</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile" activeColor="deepPink">
+      <img slot="item-icon" src="../../assets/img/tabbar/profile.svg" alt="">
+      <img slot="item-icon-active" src="../../assets/img/tabbar/profile_active.svg" alt="">
+      <div slot="item-text">商城</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile" activeColor="deepPink">
+      <img slot="item-icon" src="../../assets/img/tabbar/profile.svg" alt="">
+      <img slot="item-icon-active" src="../../assets/img/tabbar/profile_active.svg" alt="">
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+  import TabBar from 'components/tabbar/TabBar'
+  import TabBarItem from 'components/tabbar/TabBarItem'
+
+  export default {
+    name: "MainTabBar",
+    components: {
+      TabBar,
+      TabBarItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,205 @@
+<template>
+	<div class="home">
+		<div class="partOne">
+			<div class="news"></div>
+			<div class="ad_ser">
+				<div class="adress">
+						<img src="../../assets/img/partOne/icon_address.png" class="address_i"/>
+						<div class="address_c">西湖文化广场...</div>
+				</div>
+				<div class="search">
+					<div class="search_f">
+						<img src="../../assets/img/partOne/icon_search.png" class="sear_l"/>
+						<input type="text" class="sear_c" placeholder="头昏脑胀，感冒流涕"/>
+						<img src="../../assets/img/partOne/icon_camera.png" class="sear_r"/>
+					</div>
+				</div>
+			</div>
+			<div class="caidan">
+				<div class="caidanItem">
+					<img src="../../assets/img/partOne/icon_scan.png"/>
+					<div>扫一扫</div>
+				</div>
+				<div class="caidanItem">
+					<img src="../../assets/img/partOne/icon_fkm.png"/>
+					<div>付款码</div>
+				</div>
+				<div class="caidanItem">
+					<img src="../../assets/img/partOne/icon_card.png"/>
+					<div>卡券</div>
+				</div>
+				<div class="caidanItem">
+					<img src="../../assets/img/partOne/icon_sign.png"/>
+					<div>签到</div>
+				</div>
+			</div>
+		</div>
+		
+	  	<div class="partTwo">
+	  		<div class="service">
+	  			<div class="serviceItem">
+	  				<div class="serviceItem_i">
+	  					<img src="../../assets/img/partTwo/icon_wz.png"/>
+	  				</div>
+	  				<div class="serviceItem_c">免费问诊</div>
+	  			</div>
+	  			<div class="serviceItem">
+	  				<div class="serviceItem_i">
+	  					<img src="../../assets/img/partTwo/icon_zx.png"/>
+	  				</div>
+	  				<div class="serviceItem_c">药师咨询</div>
+	  			</div>
+	  			<div class="serviceItem">
+	  				<div class="serviceItem_i">
+	  					<img src="../../assets/img/partTwo/icon_gh.png"/>
+	  				</div>
+	  				<div class="serviceItem_c">预约挂号</div>
+	  			</div>
+	  			<div class="serviceItem">
+	  				<div class="serviceItem_i">
+	  					<img src="../../assets/img/partTwo/icon_gy.png"/>
+	  				</div>
+	  				<div class="serviceItem_c">送药上门</div>
+	  			</div>
+	  		</div>
+	  		<div class="service">
+	  			<div class="serviceItem">
+	  				<div class="serviceItem_i">
+	  					<img src="../../assets/img/partTwo/icon_tx.png"/>
+	  				</div>
+	  				<div class="serviceItem_c">用药提醒</div>
+	  			</div>
+	  			<div class="serviceItem">
+	  				<div class="serviceItem_i">
+	  					<img src="../../assets/img/partTwo/icon_sj.png"/>
+	  				</div>
+	  				<div class="serviceItem_c">健康数据</div>
+	  			</div>
+	  			<div class="serviceItem">
+	  				<div class="serviceItem_i">
+	  					<img src="../../assets/img/partTwo/icon_fx.png"/>
+	  				</div>
+	  				<div class="serviceItem_c">报告分析</div>
+	  			</div>
+	  			<div class="serviceItem">
+	  				<div class="serviceItem_i">
+	  					<img src="../../assets/img/partTwo/icon_cs.png"/>
+	  				</div>
+	  				<div class="serviceItem_c">健康测试</div>
+	  			</div>
+	  		</div>
+	  	</div>
+	  	
+	  	<div class="partThree">
+	  		<div class="partThree_c">
+	  			<div class="introduce_l">健康</div>
+	  			<div class="introduce_c">头条</div>
+	  			<div class="introduce_p">▎</div>
+	  			<div class="introduce_r">一招教你如何辨别流行感冒与病毒性感冒</div>
+	  		</div>
+	  	</div>
+	  	
+	  	<div class="partFour">
+	  		<div class="fourTop">
+	  			<div class="fourTop_l">
+	  					<div class="coupons">先领券 后付款</div>
+	  					<div class="coupons_b">抢100元优惠券</div>	
+	  			</div>
+	  			<div class="fourTop_r">
+	  					<div class="friend">邀请好后  送豪礼</div>
+	  					<div class="friend_b">赚积分 兑豪礼</div>
+	  			</div>
+	  		</div>
+	  		<div class="hospital">
+	  			<div class="hos_top">
+	  				<div class="hos_l">医院推荐</div>
+	  				<div class="hos_c">为你推荐合最专业适合的医院</div>
+	  				<div class="hos_r">更多</div>
+	  				<div class="hos_i">
+	  					<img class="hos_i" src="../../assets/img/partFour/icon_right .png" />
+	  				</div>
+	  			</div>
+	  			<div class="hos_body">
+	  				<div class="hosItemone">
+	  					<div class="hos_left">
+	  						<img src="../../assets/img/partFour/hospital_1.png"/>
+	  					</div>
+	  					<div class="hos_right">
+	  						<div class="hos_r_t">
+	  							<div class="hos_name">杭州市第七人民医院</div>
+	  							<div class="hos_address">6.9km</div>
+	  						</div>
+	  						<div class="hos_r_c">
+	  							<div class="hos_lable">三甲医院</div>
+	  							<div class="hos_lable">医保定点</div>
+	  							<div class="hos_lable">专科医院</div>
+	  						</div>
+	  						<div class="hos_r_b">主治：医学心理学、精神病学、精神康复学</div>
+	  					</div>
+	  				</div>
+	  				<div class="hosItemtwo">
+	  					<div class="hos_left">
+	  						<img src="../../assets/img/partFour/hospital_2.png"/>
+	  					</div>
+	  					<div class="hos_right">
+	  						<div class="hos_r_t">
+	  							<div class="hos_name">杭州市第一人民医院</div>
+	  							<div class="hos_address">6.9km</div>
+	  						</div>
+	  						<div class="hos_r_c">
+	  							<div class="hos_lable">三甲医院</div>
+	  							<div class="hos_lable">医保定点</div>
+	  							<div class="hos_lable">综合医院</div>
+	  						</div>
+	  						<div class="hos_r_b">主治：医学心理学、精神病学、精神康复学</div>
+	  					</div>
+	  				</div>
+	  			</div>
+	  		</div>
+	  		<div class="advertisement">
+	  			<div class="adv_png"></div>
+	  		</div>
+	  		<div class="popular_rec">
+	  			<div class="popular_rec_t">
+	  				<div class="hot_name">热门推荐</div>
+	  				<div class="hot_many">更多</div>
+	  				<div class="hot_i">
+	  					<img  src="../../assets/img/partFour/icon_right .png" />
+	  				</div>
+	  			</div>
+	  			<div class="popular_rec_b">
+	  				<div class="articleItem">
+	  					<div class="articleOne">
+	  						<div class="articleOne_l">
+	  							<div class="articleOne_l_t">牙疼不是病，疼起来真要命</div>
+	  							<div class="articleOne_l_b">不要再用错误的方法止痛了！</div>
+	  						</div>
+	  						<div class="articleOne_r">
+	  							<img src="../../assets/img/partFour/图层 67.png" alt="" />
+	  						</div>
+	  					</div>
+	  					<div class="articleTwo">
+	  						<div class="articleTwo_l">
+	  							<div class="articleTwo_l_t">喝酸奶比喝饮料健康？</div>
+	  							<div class="articleTwo_l_b">7种看上去健康的食品</div>
+	  						</div>
+	  						<div class="articleTwo_r">
+	  							<img src="../../assets/img/partFour/图层 64.png" alt="" />
+	  						</div>
+	  					</div>
+	  				</div>
+	  			</div>
+	  		</div>
+	  	</div>
+	</div>
+</template>
+
+<script>
+  export default {
+    name: "Home"
+   }
+</script>
+
+<style scoped>
+	@import "../../assets/css/home.css";
+</style>
